57667686ef2a7d90e6f277529ddda4f553669226
Christian Glåmseter <christiangm803@gmail.com>
2025-10-01 13:47:17 +0200

    Added swagger ui for endpoint docs

--- KollektivSystem.ApiService/Program.cs
+++ KollektivSystem.ApiService/Program.cs
@@ -15,14 +15,25 @@ builder.Services.AddProblemDetails();
 builder.AddSqlServerDbContext<ApplicationDbContext>(connectionName: "database");
 builder.Services.AddRepositories();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+        c.RoutePrefix = "";  
+    });
+}
 
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     db.Database.Migrate();
-
-    Console.WriteLine("ttt");
 
     //Example to add something to db :)
     //db.Add(new User { FirstName = "Christian", Role = KollektivSystem.ApiService.Models.Enums.Role.Developer });
