f0709dff9e78b469b82975d8b3054ff659fbb770
Zada <qadirzada.daniel@gmail.com>
2025-10-08 12:35:28 +0200

    Route class in Transport

---
+++ KollektivSystem.ApiService/Models/Transport/Route.cs
@@ -0,0 +1,9 @@
+﻿namespace KollektivSystem.ApiService.Models.Transport
+{
+    public class Route
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Stop> Stops { get; set; } = new List<Stop>();
+    }
+}
