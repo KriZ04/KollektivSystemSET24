44371af29fbacede673a88ae1e0a92de085d6e3b
Zada <qadirzada.daniel@gmail.com>
2025-10-14 16:55:32 +0200

    stop config ferdig

--- KollektivSystem.ApiService/Models/Configs/StopConfiguration.cs
+++ KollektivSystem.ApiService/Models/Configs/StopConfiguration.cs
@@ -4,7 +4,15 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace KollektivSystem.ApiService.Models.Configs
 {
-    public class StopConfiguration
+    public class StopConfiguration : IEntityTypeConfiguration<Stop>
     {
+        public void Configure(EntityTypeBuilder<Stop> builder)
+        {
+            builder.Property(s => s.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.HasIndex(s => s.Name);
+        }
     }
 }
