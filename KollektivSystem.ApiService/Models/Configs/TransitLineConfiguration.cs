d27cda62f11931f31d9f710e6dcbbe447a6d1a4d
Zada <qadirzada.daniel@gmail.com>
2025-10-14 16:45:12 +0200

    transitline config ferdig

--- KollektivSystem.ApiService/Models/Configs/TransitLineConfiguration.cs
+++ KollektivSystem.ApiService/Models/Configs/TransitLineConfiguration.cs
@@ -6,6 +6,18 @@ namespace KollektivSystem.ApiService.Models.Configs
 {
     public class TransitLineConfiguration
     {
+        public void Configure(EntityTypeBuilder<TransitLine> builder)
+        {
+            builder.Property(t => t.Name)
+                .HasMaxLength(100)
+                .IsRequired();
 
+            // Since TransitLine has many Stops
+            builder.HasMany(t => t.Stops)
+                .WithOne(s => s.Route)
+                .HasForeignKey(s => s.RouteId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
+
 }
