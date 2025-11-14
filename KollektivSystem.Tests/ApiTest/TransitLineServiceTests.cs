4d788fe23da9783d7d79a1261deb61819bde2fd0
Zada <qadirzada.daniel@gmail.com>
2025-11-14 22:01:14 +0100

    setup done for first test in transitlineservices

--- KollektivSystem.Tests/ApiTest/TransitLineServiceTests.cs
+++ KollektivSystem.Tests/ApiTest/TransitLineServiceTests.cs
@@ -17,15 +17,41 @@ namespace KollektivSystem.UnitTests.ApiTest
 {
     public class TransitLineServiceTests    {
         [Fact]
-        public void CreateAsync_WithTransitLine_ReturnsNewTransitLine()
+        public async Task CreateAsync_WithTransitLine_ReturnsNewTransitLine()
         {
             // Arrange
-           
+            var repoMock = new Mock<ITransitLineRepository>();
+            var loggerMock = new Mock<ILogger<TransitLineService>>();
+
+            var testLine = new TransitLine
+            {
+                Id = 1,
+                Name = "Test Line",
+                Stops = new List<Stop>()
+            };
+
+            repoMock.Setup(r => r.AddAsync(It.IsAny<TransitLine>(), default))
+                    .Returns(Task.CompletedTask);
+
+            repoMock.Setup(r => r.SaveChanges())
+                    .Returns(Task.CompletedTask);
+
+            var service = new TransitLineService(repoMock.Object, loggerMock.Object);
 
             // Act
+            var result = await service.CreateAsync(testLine);
 
 
             // Assert
+            result.Should().NotBeNull();
+            result.Should().Be(testLine);
+            result.Name.Should().Be("Test Line");
+
+            repoMock.Verify(r => r.AddAsync(It.IsAny<TransitLine>(), default), Times.Once);
+            repoMock.Verify(r => r.SaveChanges(), Times.Once);
+
+
+
         }
 
     }
