1b09ae95d5effb8fdbcc7c9997c66704c138a871
Isak Valdersnes Olsvold <isakvo@hiof.no>
2025-11-20 00:06:31 +0100

    added Creat and response Dtos for TransitLineStop

---
+++ KollektivSystem.ApiService/Models/Dtos/TransitLineStops/TransitLineStopResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace KollektivSystem.ApiService.Models.Dtos.TransitLineStops
+{
+    public sealed class TransitLineStopResponse
+    {
+        public int Id { get; init; }
+        public int Order { get; init; }
+        public int TransitLineId { get; init; }
+        public int StopId { get; init; }
+    }
+}
